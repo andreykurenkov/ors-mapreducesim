cac32287888f59f9f4e29023885b1e703a7e4b1f
Andrey.Sendme@gmail.com <Andrey.Sendme@gmail.com@d5ea6c9b-9ce2-9812-45da-a14e545efa23>
2013-01-21 03:44:36 +0000

    Sharing project "MapReduceSim" into "https://ors-mapreducesim.googlecode.com/svn/trunk". This an eclipse project shared with subclipse to hopefully make management easy.

---
+++ trunk/MapReduceSim/src/MapReduceSim/MapReduceSimMain.java
@@ -0,0 +1,35 @@
+/*
+ *  Generated Main Class. Most user won't have to modify it.
+ */
+package MapReduceSim;
+import org.simgrid.msg.Msg;
+import org.simgrid.msg.NativeException;
+ 
+public class MapReduceSimMain  {
+    
+    public static void main(String[] args) throws NativeException {    
+        /* check usage error and initialize with defaults */
+        if (args.length == 0){
+            args = new String[2];
+            System.out.print("** WARNING **\nusing default values:\n"+
+                "MapReduceSim_platform.xml MapReduceSim_deployment.xml\n\n");
+            args[0] = "MapReduceSim_platform.xml";
+            args[1] = "MapReduceSim_deployment.xml";
+        }else if(args.length != 2) {
+            System.out.print("** ERROR **\n"+
+                "Usage:\nplatform_file deployment_file\n");
+            System.out.print("Example:\nMapReduceSim_platform.xml MapReduceSim_deployment.xml\n");
+            System.exit(1);
+        }	
+	    /* initialize the MSG simulation. Must be done before anything else (even logging). */
+        Msg.init(args);
+        Msg.info("Simulation start...");
+	    /* construct the platform and deploy the application */
+        Msg.createEnvironment(args[0]);
+        Msg.deployApplication(args[1]);
+	    
+	    /*  execute the simulation. */
+        Msg.run();
+        Msg.info("Simulation time:"+Msg.getClock());
+    }
+}
